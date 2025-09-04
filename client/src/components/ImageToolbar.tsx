3b537b196224e3b03a20e1a2a649e8e570e97192
ammarqd <ammarqd@outlook.com>
2025-09-04 19:53:36 +0100

    Add new icons and improved sidebar

--- client/src/components/ImageToolbar.tsx
+++ client/src/components/ImageToolbar.tsx
@@ -12,6 +12,8 @@ export function ImageToolbar({ onNewImage, onUndo, onRedo }: ImageToolbarProps) 
   return (
     <div className="flex items-center justify-center pt-3 pb-3 px-4">
       <div className="flex items-center items-stretch divide-x px-2 border border-neutral-300 rounded-lg shadow-md bg-white" >
+        
+        {/* New Image */}
         <button 
           className="flex items-center py-2 px-3 gap-2 text-neutral-900 cursor-pointer hover:bg-neutral-50 transition-colors text-sm font-medium"
           onClick={onNewImage}
@@ -41,40 +43,43 @@ export function ImageToolbar({ onNewImage, onUndo, onRedo }: ImageToolbarProps) 
           New Image
         </button>
         
+        {/* Undo */}
         <button 
           className="flex items-center py-2 px-3 text-neutral-900 cursor-pointer hover:bg-neutral-50 transition-colors text-sm font-medium"
           onClick={onUndo}
           title="Undo"
         >
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            className="w-4 h-4" 
-            viewBox="0 0 24 24" 
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9 14l-5-5m0 0l5-5m-5 5h12a4 4 0 014 4v1"/>
+          <svg className="w-3 h-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_202_92)">
+              <path d="M9.95311 10.5156H0.5625C0.251859 10.5156 0 10.2637 0 9.95311V0.5625C0 0.251859 0.251859 0 0.5625 0H2.8125C3.12314 0 3.375 0.251859 3.375 0.5625V4.224C5.52061 1.8412 8.63719 0.350156 12.102 0.375328C18.5194 0.421922 23.6542 5.60766 23.6406 12.0253C23.6269 18.434 18.4275 23.625 12.0156 23.625C9.01144 23.625 6.27361 22.4853 4.21045 20.6149C3.97144 20.3983 3.96042 20.0265 4.18856 19.7984L5.78077 18.2062C5.99048 17.9964 6.32742 17.985 6.54956 18.1815C8.00438 19.4689 9.91781 20.25 12.0156 20.25C16.575 20.25 20.2656 16.5602 20.2656 12C20.2656 7.44061 16.5758 3.75 12.0156 3.75C9.27361 3.75 6.84623 5.08481 5.34652 7.14061H9.95311C10.2638 7.14061 10.5156 7.39247 10.5156 7.70311V9.95311C10.5156 10.2637 10.2638 10.5156 9.95311 10.5156V10.5156Z" fill="black"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_202_92">
+                <rect width="24" height="24" fill="white"/>
+              </clipPath>
+            </defs>
           </svg>
         </button>
         
+        {/* Redo */}
         <button 
           className="flex items-center py-2 px-3 text-neutral-900 cursor-pointer hover:bg-neutral-50 transition-colors text-sm font-medium" 
           onClick={onRedo}
           title="Redo"
         >
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            className="w-4 h-4" 
-            viewBox="0 0 24 24" 
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 14l5-5m0 0l-5-5m5 5H8a4 4 0 00-4 4v1"/>
+          <svg className="w-3 h-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_202_90)">
+              <path d="M23.453 3.58398e-07H21.2306C21.1545 -8.56237e-05 21.0791 0.0153 21.009 0.0452243C20.939 0.0751486 20.8757 0.118988 20.8231 0.174084C20.7705 0.229181 20.7297 0.294386 20.703 0.365746C20.6764 0.437105 20.6645 0.513133 20.6681 0.589219L20.8556 4.46859C19.7655 3.18398 18.4085 2.1523 16.8792 1.44535C15.3498 0.7384 13.6848 0.373145 12 0.375C5.59407 0.375 0.370316 5.60297 0.375003 12.0089C0.379691 18.4252 5.58282 23.625 12 23.625C14.8786 23.6291 17.6556 22.5611 19.7897 20.6292C19.8467 20.5782 19.8927 20.5161 19.9249 20.4467C19.9571 20.3773 19.9748 20.302 19.9769 20.2255C19.979 20.1491 19.9654 20.073 19.9371 20.0019C19.9088 19.9308 19.8663 19.8663 19.8122 19.8122L18.2184 18.2184C18.1176 18.1176 17.9822 18.0588 17.8396 18.0541C17.6971 18.0493 17.558 18.0988 17.4506 18.1927C16.1913 19.3006 14.6242 19.9984 12.9582 20.193C11.2922 20.3876 9.60644 20.0697 8.12569 19.2818C6.64495 18.4939 5.43954 17.2733 4.67017 15.7828C3.90081 14.2924 3.60403 12.6028 3.81941 10.9393C4.03479 9.27589 4.75209 7.71761 5.87569 6.47223C6.99929 5.22685 8.4758 4.35355 10.1084 3.96873C11.741 3.58392 13.4521 3.70588 15.0136 4.31836C16.5751 4.93083 17.9128 6.00473 18.8484 7.39688L14.0892 7.16859C14.0131 7.16498 13.9371 7.17685 13.8657 7.20349C13.7944 7.23014 13.7292 7.271 13.6741 7.32359C13.619 7.37619 13.5752 7.43943 13.5452 7.50948C13.5153 7.57953 13.4999 7.65492 13.5 7.73109V9.95344C13.5 10.1026 13.5593 10.2457 13.6648 10.3512C13.7702 10.4567 13.9133 10.5159 14.0625 10.5159H23.453C23.6022 10.5159 23.7452 10.4567 23.8507 10.3512C23.9562 10.2457 24.0155 10.1026 24.0155 9.95344V0.5625C24.0155 0.413316 23.9562 0.270242 23.8507 0.164753C23.7452 0.0592636 23.6022 3.58398e-07 23.453 3.58398e-07V3.58398e-07Z" fill="black"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_202_90">
+                <rect width="24" height="24" fill="white"/>
+              </clipPath>
+            </defs>
           </svg>
         </button>
         
+        {/* Show Original */}
         <button 
           className="flex items-center py-2 px-3 gap-2 text-neutral-900 cursor-pointer hover:bg-neutral-50 transition-colors text-sm font-medium"
           onMouseUp={() => setShowOriginal(false)}
@@ -90,6 +95,27 @@ export function ImageToolbar({ onNewImage, onUndo, onRedo }: ImageToolbarProps) 
           </svg>
           Hold to Show Original
         </button>
+
+        {/* Save */}
+        <button
+          className="flex items-center py-2 px-3 gap-2 text-neutral-900 cursor-pointer hover:bg-neutral-50 transition-colors text-sm font-medium"
+          onClick={() => {}} // placeholder save logic
+          title="Save"
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="w-4 h-4"
+            viewBox="0 0 24 24" 
+            fill="none"
+          >
+            <path 
+              d="M20.71 9.29L14.71 3.29C14.6178 3.20005 14.5092 3.12874 14.39 3.08C14.266 3.02962 14.1338 3.0025 14 3H6C5.20435 3 4.44129 3.31607 3.87868 3.87868C3.31607 4.44129 3 5.20435 3 6V18C3 18.7956 3.31607 19.5587 3.87868 20.1213C4.44129 20.6839 5.20435 21 6 21H18C18.7956 21 19.5587 20.6839 20.1213 20.1213C20.6839 19.5587 21 18.7956 21 18V10C21.0008 9.86839 20.9755 9.73793 20.9258 9.61609C20.876 9.49426 20.8027 9.38344 20.71 9.29ZM9 5H13V7H9V5ZM15 19H9V16C9 15.7348 9.10536 15.4804 9.29289 15.2929C9.48043 15.1054 9.73478 15 10 15H14C14.2652 15 14.5196 15.1054 14.7071 15.2929C14.8946 15.4804 15 15.7348 15 16V19ZM19 18C19 18.2652 18.8946 18.5196 18.7071 18.7071C18.5196 18.8946 18.2652 19 18 19H17V16C17 15.2044 16.6839 14.4413 16.1213 13.8787C15.5587 13.3161 14.7956 13 14 13H10C9.20435 13 8.44129 13.3161 7.87868 13.8787C7.31607 14.4413 7 15.2044 7 16V19H6C5.73478 19 5.48043 18.8946 5.29289 18.7071C5.10536 18.5196 5 18.2652 5 18V6C5 5.73478 5.10536 5.48043 5.29289 5.29289C5.48043 5.10536 5.73478 5 6 5H7V8C7 8.26522 7.10536 8.51957 7.29289 8.70711C7.48043 8.89464 7.73478 9 8 9H14C14.2652 9 14.5196 8.89464 14.7071 8.70711C14.8946 8.51957 15 8.26522 15 8V6.41L19 10.41V18Z" 
+              fill="black"
+            />
+          </svg>
+          Save
+        </button>
+
       </div>
     </div>
   )
--- client/src/components/SidebarControls.tsx
+++ client/src/components/SidebarControls.tsx
@@ -7,133 +7,158 @@ export function SidebarControls() {
   const [saturation, setSaturation] = useState(100)
 
   return (
-    <aside className="bg-white border-r border-neutral-300 flex flex-col w-80 rounded-xl">
-      <div className="p-8 flex flex-col gap-3">
-        <h3 className="text-base font-semibold m-0 pb-2 border-b border-neutral-300 text-neutral-900">
-          Colours
-        </h3>
+    <aside className="bg-white border-r border-neutral-300 flex flex-col w-80 shadow-lg z-99">
+      <div className="py-8 px-6 flex flex-col gap-3">
 
-        {/* Hue */}
-        <div className="flex flex-col">
-          <label htmlFor="hue" className="text-sm font-medium text-neutral-900">
-            Hue
-          </label>
-          <div className="flex items-center gap-2">
-            <input
-              type="range"
-              id="hue"
-              min="0"
-              max="360"
-              value={hue}
-              onChange={(e) => setHue(Number(e.target.value))}
-              className="h-2 rounded cursor-pointer appearance-none flex-1 slider-hue"
-            />
-            <input
-              type="number"
-              min={0}
-              max={360}
-              value={hue}
-              onChange={(e) => {
-                let val = Number(e.target.value)
-                if (val > 360) val = 360
-                if (val < 0) val = 0
-                setHue(val)
-              }}
-              className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
-            />
-          </div>
+        <div className="flex items-center gap-3 pb-2 border-b border-neutral-300">
+            
+            <svg className="h-5 w-5 text-neutral-900" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(90)">
+            <path d="M19.9999 8.18V3C19.9999 2.73478 19.8946 2.48043 19.707 2.29289C19.5195 2.10536 19.2651 2 18.9999 2C18.7347 2 18.4803 2.10536 18.2928 2.29289C18.1053 2.48043 17.9999 2.73478 17.9999 3V8.18C17.4208 8.3902 16.9204 8.77363 16.5668 9.27816C16.2131 9.7827 16.0234 10.3839 16.0234 11C16.0234 11.6161 16.2131 12.2173 16.5668 12.7218C16.9204 13.2264 17.4208 13.6098 17.9999 13.82V21C17.9999 21.2652 18.1053 21.5196 18.2928 21.7071C18.4803 21.8946 18.7347 22 18.9999 22C19.2651 22 19.5195 21.8946 19.707 21.7071C19.8946 21.5196 19.9999 21.2652 19.9999 21V13.82C20.5791 13.6098 21.0795 13.2264 21.4331 12.7218C21.7867 12.2173 21.9764 11.6161 21.9764 11C21.9764 10.3839 21.7867 9.7827 21.4331 9.27816C21.0795 8.77363 20.5791 8.3902 19.9999 8.18V8.18ZM18.9999 12C18.8021 12 18.6088 11.9414 18.4443 11.8315C18.2799 11.7216 18.1517 11.5654 18.076 11.3827C18.0003 11.2 17.9805 10.9989 18.0191 10.8049C18.0577 10.6109 18.153 10.4327 18.2928 10.2929C18.4327 10.153 18.6108 10.0578 18.8048 10.0192C18.9988 9.98063 19.1999 10.0004 19.3826 10.0761C19.5653 10.1518 19.7215 10.28 19.8314 10.4444C19.9413 10.6089 19.9999 10.8022 19.9999 11C19.9999 11.2652 19.8946 11.5196 19.707 11.7071C19.5195 11.8946 19.2651 12 18.9999 12ZM12.9999 14.18V3C12.9999 2.73478 12.8946 2.48043 12.707 2.29289C12.5195 2.10536 12.2651 2 11.9999 2C11.7347 2 11.4803 2.10536 11.2928 2.29289C11.1053 2.48043 10.9999 2.73478 10.9999 3V14.18C10.4208 14.3902 9.92037 14.7736 9.56676 15.2782C9.21314 15.7827 9.02344 16.3839 9.02344 17C9.02344 17.6161 9.21314 18.2173 9.56676 18.7218C9.92037 19.2264 10.4208 19.6098 10.9999 19.82V21C10.9999 21.2652 11.1053 21.5196 11.2928 21.7071C11.4803 21.8946 11.7347 22 11.9999 22C12.2651 22 12.5195 21.8946 12.707 21.7071C12.8946 21.5196 12.9999 21.2652 12.9999 21V19.82C13.5791 19.6098 14.0795 19.2264 14.4331 18.7218C14.7867 18.2173 14.9764 17.6161 14.9764 17C14.9764 16.3839 14.7867 15.7827 14.4331 15.2782C14.0795 14.7736 13.5791 14.3902 12.9999 14.18ZM11.9999 18C11.8021 18 11.6088 17.9414 11.4443 17.8315C11.2799 17.7216 11.1517 17.5654 11.076 17.3827C11.0003 17.2 10.9805 16.9989 11.0191 16.8049C11.0577 16.6109 11.153 16.4327 11.2928 16.2929C11.4327 16.153 11.6108 16.0578 11.8048 16.0192C11.9988 15.9806 12.1999 16.0004 12.3826 16.0761C12.5653 16.1518 12.7215 16.28 12.8314 16.4444C12.9413 16.6089 12.9999 16.8022 12.9999 17C12.9999 17.2652 12.8946 17.5196 12.707 17.7071C12.5195 17.8946 12.2651 18 11.9999 18ZM5.99991 6.18V3C5.99991 2.73478 5.89456 2.48043 5.70702 2.29289C5.51948 2.10536 5.26513 2 4.99991 2C4.7347 2 4.48034 2.10536 4.29281 2.29289C4.10527 2.48043 3.99991 2.73478 3.99991 3V6.18C3.42076 6.3902 2.92037 6.77363 2.56676 7.27817C2.21314 7.7827 2.02344 8.38388 2.02344 9C2.02344 9.61612 2.21314 10.2173 2.56676 10.7218C2.92037 11.2264 3.42076 11.6098 3.99991 11.82V21C3.99991 21.2652 4.10527 21.5196 4.29281 21.7071C4.48034 21.8946 4.7347 22 4.99991 22C5.26513 22 5.51948 21.8946 5.70702 21.7071C5.89456 21.5196 5.99991 21.2652 5.99991 21V11.82C6.57907 11.6098 7.07945 11.2264 7.43307 10.7218C7.78669 10.2173 7.97639 9.61612 7.97639 9C7.97639 8.38388 7.78669 7.7827 7.43307 7.27817C7.07945 6.77363 6.57907 6.3902 5.99991 6.18V6.18ZM4.99991 10C4.80213 10 4.60879 9.94135 4.44434 9.83147C4.27989 9.72159 4.15172 9.56541 4.07603 9.38268C4.00035 9.19996 3.98054 8.99889 4.01913 8.80491C4.05771 8.61093 4.15296 8.43275 4.29281 8.29289C4.43266 8.15304 4.61084 8.0578 4.80482 8.01921C4.9988 7.98063 5.19987 8.00043 5.3826 8.07612C5.56532 8.15181 5.7215 8.27998 5.83138 8.44443C5.94127 8.60888 5.99991 8.80222 5.99991 9C5.99991 9.26522 5.89456 9.51957 5.70702 9.70711C5.51948 9.89464 5.26513 10 4.99991 10Z" fill="black"/>
+          </svg>
+
+          <h2 className="text-base font-semibold text-neutral-900">Filters</h2>
         </div>
 
-        {/* Brightness */}
-        <div className="flex flex-col">
-          <label htmlFor="brightness" className="text-sm font-medium text-neutral-900">
-            Brightness
-          </label>
-          <div className="flex items-center gap-2">
-            <input
-              type="range"
-              id="brightness"
-              min="0"
-              max="200"
-              value={brightness}
-              onChange={(e) => setBrightness(Number(e.target.value))}
-              className="h-2 rounded cursor-pointer appearance-none flex-1 slider-brightness"
+        <div className="flex items-center gap-3 mt-4">
+          <svg
+            className="h-4 w-4 text-neutral-900 flex-shrink-0"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              d="M12 22C10.6868 22 9.38642 21.7413 8.17317 21.2388C6.95991 20.7362 5.85752 19.9997 4.92893 19.0711C3.05357 17.1957 2 14.6522 2 12C2 9.34784 3.05357 6.8043 4.92893 4.92893C6.8043 3.05357 9.34784 2 12 2C17.5 2 22 6 22 11C22 12.5913 21.3679 14.1174 20.2426 15.2426C19.1174 16.3679 17.5913 17 16 17H14.2C13.9 17 13.7 17.2 13.7 17.5C13.7 17.6 13.8 17.7 13.8 17.8C14.2 18.3 14.4 18.9 14.4 19.5C14.5 20.9 13.4 22 12 22ZM12 4C9.87827 4 7.84344 4.84285 6.34315 6.34315C4.84285 7.84344 4 9.87827 4 12C4 14.1217 4.84285 16.1566 6.34315 17.6569C7.84344 19.1571 9.87827 20 12 20C12.3 20 12.5 19.8 12.5 19.5C12.5 19.3 12.4 19.2 12.4 19.1C12 18.6 11.8 18.1 11.8 17.5C11.8 16.1 12.9 15 14.3 15H16C17.0609 15 18.0783 14.5786 18.8284 13.8284C19.5786 13.0783 20 12.0609 20 11C20 7.1 16.4 4 12 4ZM6.5 10C7.3 10 8 10.7 8 11.5C8 12.3 7.3 13 6.5 13C5.7 13 5 12.3 5 11.5C5 10.7 5.7 10 6.5 10ZM9.5 6C10.3 6 11 6.7 11 7.5C11 8.3 10.3 9 9.5 9C8.7 9 8 8.3 8 7.5C8 6.7 8.7 6 9.5 6ZM14.5 6C15.3 6 16 6.7 16 7.5C16 8.3 15.3 9 14.5 9C13.7 9 13 8.3 13 7.5C13 6.7 13.7 6 14.5 6ZM17.5 10C18.3 10 19 10.7 19 11.5C19 12.3 18.3 13 17.5 13C16.7 13 16 12.3 16 11.5C16 10.7 16.7 10 17.5 10Z"
+              fill="currentColor"
             />
-            <input
-              type="number"
-              min={0}
-              max={200}
-              value={brightness}
-              onChange={(e) => {
-                let val = Number(e.target.value)
-                if (val > 200) val = 200
-                if (val < 0) val = 0
-                setBrightness(val)
-              }}
-              className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
-            />
-          </div>
+          </svg>
+
+          <h3 id="colours-heading" className="text-sm font-semibold text-neutral-900">Colours</h3>
         </div>
 
-        {/* Contrast */}
-        <div className="flex flex-col">
-          <label htmlFor="contrast" className="text-sm font-medium text-neutral-900">
-            Contrast
-          </label>
-          <div className="flex items-center gap-2">
-            <input
-              type="range"
-              id="contrast"
-              min="0"
-              max="200"
-              value={contrast}
-              onChange={(e) => setContrast(Number(e.target.value))}
-              className="h-2 rounded cursor-pointer appearance-none flex-1 slider-contrast"
-            />
-            <input
-              type="number"
-              min={0}
-              max={200}
-              value={contrast}
-              onChange={(e) => {
-                let val = Number(e.target.value)
-                if (val > 200) val = 200
-                if (val < 0) val = 0
-                setContrast(val)
-              }}
-              className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
-            />
+        <div className="flex flex-col" aria-labelledby="colours-heading">
+          <div className="flex flex-col">
+            <label htmlFor="hue" className="text-sm font-medium text-neutral-900">
+              Hue
+            </label>
+            <div className="flex items-center gap-2">
+              <input
+                type="range"
+                id="hue"
+                min="0"
+                max="360"
+                value={hue}
+                onChange={(e) => setHue(Number(e.target.value))}
+                className="h-2 rounded cursor-pointer appearance-none flex-1 slider-hue"
+              />
+              <input
+                type="number"
+                min={0}
+                max={360}
+                value={hue}
+                onChange={(e) => {
+                  let val = Number(e.target.value)
+                  if (Number.isNaN(val)) val = 0
+                  if (val > 360) val = 360
+                  if (val < 0) val = 0
+                  setHue(val)
+                }}
+                className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Saturation */}
-        <div className="flex flex-col">
-          <label htmlFor="saturation" className="text-sm font-medium text-neutral-900">
-            Saturation
-          </label>
-          <div className="flex items-center gap-2">
-            <input
-              type="range"
-              id="saturation"
-              min="0"
-              max="200"
-              value={saturation}
-              onChange={(e) => setSaturation(Number(e.target.value))}
-              className="h-2 rounded cursor-pointer appearance-none flex-1 slider-saturation"
-            />
-            <input
-              type="number"
-              min={0}
-              max={200}
-              value={saturation}
-              onChange={(e) => {
-                let val = Number(e.target.value)
-                if (val > 200) val = 200
-                if (val < 0) val = 0
-                setSaturation(val)
-              }}
-              className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
-            />
+          <div className="flex flex-col">
+            <label htmlFor="brightness" className="text-sm font-medium text-neutral-900">
+              Brightness
+            </label>
+            <div className="flex items-center gap-2">
+              <input
+                type="range"
+                id="brightness"
+                min="0"
+                max="200"
+                value={brightness}
+                onChange={(e) => setBrightness(Number(e.target.value))}
+                className="h-2 rounded cursor-pointer appearance-none flex-1 slider-brightness"
+              />
+              <input
+                type="number"
+                min={0}
+                max={200}
+                value={brightness}
+                onChange={(e) => {
+                  let val = Number(e.target.value)
+                  if (Number.isNaN(val)) val = 0
+                  if (val > 200) val = 200
+                  if (val < 0) val = 0
+                  setBrightness(val)
+                }}
+                className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="contrast" className="text-sm font-medium text-neutral-900">
+              Contrast
+            </label>
+            <div className="flex items-center gap-2">
+              <input
+                type="range"
+                id="contrast"
+                min="0"
+                max="200"
+                value={contrast}
+                onChange={(e) => setContrast(Number(e.target.value))}
+                className="h-2 rounded cursor-pointer appearance-none flex-1 slider-contrast"
+              />
+              <input
+                type="number"
+                min={0}
+                max={200}
+                value={contrast}
+                onChange={(e) => {
+                  let val = Number(e.target.value)
+                  if (Number.isNaN(val)) val = 0
+                  if (val > 200) val = 200
+                  if (val < 0) val = 0
+                  setContrast(val)
+                }}
+                className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="saturation" className="text-sm font-medium text-neutral-900">
+              Saturation
+            </label>
+            <div className="flex items-center gap-2">
+              <input
+                type="range"
+                id="saturation"
+                min="0"
+                max="200"
+                value={saturation}
+                onChange={(e) => setSaturation(Number(e.target.value))}
+                className="h-2 rounded cursor-pointer appearance-none flex-1 slider-saturation"
+              />
+              <input
+                type="number"
+                min={0}
+                max={200}
+                value={saturation}
+                onChange={(e) => {
+                  let val = Number(e.target.value)
+                  if (Number.isNaN(val)) val = 0
+                  if (val > 200) val = 200
+                  if (val < 0) val = 0
+                  setSaturation(val)
+                }}
+                className="w-12 text-center text-xs font-semibold text-neutral-900 bg-neutral-100 border border-neutral-300 rounded"
+              />
+            </div>
           </div>
         </div>
       </div>
