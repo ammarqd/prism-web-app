1632cdb8ddfc5e1bb33ab21da8a32287b441acc9
ammarqd <ammarqd@outlook.com>
2025-09-26 17:42:25 +0100

    Update to use env variable for PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth'
 
 dotenv.config()
 
 const app = express()
-
 app.use(express.json())
-
 app.use('/api/auth', authRoutes)
 
-const PORT = 4000
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
